72f5b4c3ea99418b977e6c74b34dd1b008638a20
morrow1nd <morrow1nd@yahoo.com>
2018-04-28 16:36:17 +0800

    String

--- Source/CMakeSources.cmake
+++ Source/CMakeSources.cmake
@@ -7,29 +7,27 @@ SET(TOY_UTILITY_SRC_PREREQUISITES
 )
 
 SET(TOY_UTILITY_INC_CONTAINER
-    "${TOY_UTILITY_INCLUDE_DIR}/ToyUtility/Container/String.h"
+
 )
 
 SET(TOY_UTILITY_SRC_CONTAINER
     "test.cpp"
 )
 
+SET(TOY_UTILITY_INC_STRING
+    "${TOY_UTILITY_INCLUDE_DIR}/ToyUtility/String/String.h"
+)
+
+SET(TOY_UTILITY_SRC_STRING)
+
 
 # if(MSVC)
 #   list(APPEND BS_BANSHEECORE_INC_PLATFORM VSVisualizations.natvis)
 # endif()
 
-# if(WIN32)
-#   list(APPEND BS_BANSHEECORE_INC_PLATFORM ${BS_BANSHEECORE_INC_PLATFORM_WIN32})
-#   list(APPEND BS_BANSHEECORE_SRC_PLATFORM ${BS_BANSHEECORE_SRC_PLATFORM_WIN32})
-# elseif(LINUX)
-#   list(APPEND BS_BANSHEECORE_INC_PLATFORM ${BS_BANSHEECORE_INC_PLATFORM_LINUX})
-#   list(APPEND BS_BANSHEECORE_SRC_PLATFORM ${BS_BANSHEECORE_SRC_PLATFORM_LINUX})
-# elseif(APPLE)
-#   list(APPEND BS_BANSHEECORE_INC_PLATFORM ${BS_BANSHEECORE_INC_PLATFORM_MACOS})
-#   list(APPEND BS_BANSHEECORE_SRC_PLATFORM ${BS_BANSHEECORE_SRC_PLATFORM_MACOS})
-# endif()
 
+source_group("Header Files\\String"                     FILES ${TOY_UTILITY_INC_STRING})
+source_group("Source Files\\String"                     FILES ${TOY_UTILITY_SRC_STRING})
 source_group("Header Files\\Container"                  FILES ${TOY_UTILITY_INC_CONTAINER})
 source_group("Source Files\\Container"                  FILES ${TOY_UTILITY_SRC_CONTAINER})
 source_group("Header Files\\Prerequisites"              FILES ${TOY_UTILITY_INC_PREREQUISITES})
@@ -39,6 +37,8 @@ source_group("CMake"                                    FILES "CMakeSources.cmak
 
 set(TOY_UTILITY_SRC
     "CMakeSources.cmake" "CMakeLists.txt"
+    ${TOY_UTILITY_INC_STRING}
+    ${TOY_UTILITY_SRC_STRING}
     ${TOY_UTILITY_INC_CONTAINER}
     ${TOY_UTILITY_SRC_CONTAINER}
     ${TOY_UTILITY_INC_PREREQUISITES}
