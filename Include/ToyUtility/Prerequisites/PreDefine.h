8208acd17cbd7e6960077e82299d261201f83fdc
morrow1nd <morrow1nd@yahoo.com>
2018-04-29 23:48:33 +0800

    Add UnorderedMap.h

--- Include/ToyUtility/Prerequisites/PreDefine.h
+++ Include/ToyUtility/Prerequisites/PreDefine.h
@@ -2,9 +2,19 @@
 
 #include "ToyUtility/Prerequisites/Types.h"
 
+#ifdef _WIN32
+#  undef min
+#  undef max
+#	if !defined(NOMINMAX) && defined(_MSC_VER)
+#		define NOMINMAX // required to stop windows.h messing up std::min
+#	endif
+#  if defined( __MINGW32__ )
+#    include <unistd.h>
+#  endif
+#endif
+
 
 namespace ToyUtility
 {
-
 
 } // end of namespace ToyUtility
